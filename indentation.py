3330894b3082d344e25e50d3d89a6eb0b5b9f0e4
Sania Kurup <50150839+saniakurup@users.noreply.github.com>
2024-03-28 18:34:19 +0530

    Create indentation.py

---
+++ indentation.py
@@ -0,0 +1,9 @@
+if True:
+    print("This line is indented and belongs to the if block.")
+    if False:
+        print("This line is indented and belongs to the inner if block.")
+    else:
+        print("This line is indented and belongs to the else block.")
+print("This line is not indented and is outside the if block.")
+
+
