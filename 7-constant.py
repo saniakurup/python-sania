a329f3bbac1885086b5d89d1e94427e52170139e
Sania Kurup <50150839+saniakurup@users.noreply.github.com>
2024-03-28 18:40:30 +0530

    Create 7-constant.py

---
+++ 7-constant.py
@@ -0,0 +1,11 @@
+# Constants (using uppercase names)
+PI = 3.14159
+GRAVITY = 9.8
+
+# Printing constants
+print("Value of PI:", PI)
+print("Value of GRAVITY:", GRAVITY)
+
+# Attempting to change the value of a "constant"
+PI = 3.14  # This will generate a warning, but it's not prevented by the language
+print("Updated value of PI:", PI)
