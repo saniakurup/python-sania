05618090920e08082da61da77ec90dd77bae8600
Sania Kurup <50150839+saniakurup@users.noreply.github.com>
2024-04-02 16:08:56 +0530

    Create 8-datatypes.py

---
+++ 8-datatypes.py
@@ -0,0 +1,71 @@
+# Integer
+integer_var = 42
+print("Integer:", integer_var)
+
+# Float
+float_var = 3.14
+print("Float:", float_var)
+
+# String
+string_var = "Hello, world!"
+print("String:", string_var)
+
+# Boolean
+boolean_var = True
+print("Boolean:", boolean_var)
+
+# List
+list_var = [1, 2, 3, 4, 5]
+print("List:", list_var)
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+print("Tuple:", tuple_var)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+print("Set:", set_var)
+
+# Dictionary
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary:", dict_var)
+
+# NoneType
+none_var = None
+print("None:", none_var)
+
+# Complex
+complex_var = 2 + 3j
+print("Complex:", complex_var)
+
+# Bytes
+bytes_var = b"Hello"
+print("Bytes:", bytes_var)
+
+# Bytearray
+bytearray_var = bytearray(b"Hello")
+print("Bytearray:", bytearray_var)
+
+# Range
+range_var = range(5)
+print("Range:", list(range_var))
+
+# Frozenset
+frozenset_var = frozenset({1, 2, 3, 4, 5})
+print("Frozenset:", frozenset_var)
+
+# Type of variables
+print("Type of integer_var:", type(integer_var))
+print("Type of float_var:", type(float_var))
+print("Type of string_var:", type(string_var))
+print("Type of boolean_var:", type(boolean_var))
+print("Type of list_var:", type(list_var))
+print("Type of tuple_var:", type(tuple_var))
+print("Type of set_var:", type(set_var))
+print("Type of dict_var:", type(dict_var))
+print("Type of none_var:", type(none_var))
+print("Type of complex_var:", type(complex_var))
+print("Type of bytes_var:", type(bytes_var))
+print("Type of bytearray_var:", type(bytearray_var))
+print("Type of range_var:", type(range_var))
+print("Type of frozenset_var:", type(frozenset_var))
